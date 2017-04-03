68f2afd33abb8a99a67a9cd5350a00fdc883b9c7
Jose Caceres <josecaceres.oreul@gmail.com>
2017-04-02 20:45:47 -0400

    Update GameController.cpp

--- Project3/GameController.cpp
+++ Project3/GameController.cpp
@@ -6,7 +6,6 @@ GameController::GameController(void) {}
 GameController::GameController(GameView * gameView, GameModel * gameModel) {
 	this->gameView = gameView;
 	this->gameModel = gameModel;
-
 	gameView->asignData(gameModel->getData());
 	running = true;
 	run();
@@ -17,7 +16,6 @@ GameController::~GameController(void) {}
 void GameController::run() {
 	while(running) {
 		running = gameView->run();
-		
 		if(gameView->isApplyIA()) {
 			gameModel->applyIA();
 			gameView->insertDataIA(gameModel->getI0(), gameModel->getJ0(), gameModel->getI1(), gameModel->getJ1());
