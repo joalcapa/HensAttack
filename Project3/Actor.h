2729176f729736fdfba326fcd3916621df67903f
Jose Caceres <josecaceres.oreul@gmail.com>
2017-04-02 20:28:14 -0400

    Create Actor.h

---
+++ Project3/Actor.h
@@ -0,0 +1,37 @@
+#include "Imagen.h"
+
+#pragma once
+class Actor {
+private:
+	Imagen * image;
+	Actor * actorSuccessor;
+	int typeAction;
+	int typeBody;
+	int X;
+	int Y;
+	char * route;
+	bool isRendering;
+
+public:
+	const static int ACTION_NULL = 0;
+	const static int ACTION_MOVE_TO = 1;
+	const static int BODY_FOX = 8;
+	const static int BODY_HEN = 9;
+	const static int BODY_OBJECT = 10;
+
+	Actor(void);
+	~Actor(void);
+	Actor(SDL_Renderer * renderer, const char * ruta, int x, int y, int typeBody);
+	
+	void setActorSuccessor(Actor * actorSuccessor);
+	void update();
+	void render();
+	void addAction(int typeAction, int x, int y);
+	void addAction(int typeAction, char * route);
+	void setIsRendering(bool isRendering);
+
+	Actor * getActorSuccessor();
+	int getX();
+	int getY();
+	int getTypeAction();
+};
